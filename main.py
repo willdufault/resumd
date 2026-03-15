f80114629951bbf98f18d621b8f3f208123842f1
wduf <wduf02@gmail.com>
2026-03-14 22:35:08 -0400

    refactor(parse): refactor main to call resume_pdf

--- main.py
+++ main.py
@@ -1,45 +1,42 @@
-import sys
+import yaml
+
+from models.resume_pdf import ResumePdf
 
 # TODO: read paths from sys.argv
-RESUME_PATH = "test_resume.md"
-TEMPLATE_PATH = "templates/will.yaml"
+RESUME_PATH = "resume.md"
+TEMPLATE_PATH = "templates/default.yaml"
 
 with open(RESUME_PATH) as file:
     resume_lines = file.readlines()
 
+with open(TEMPLATE_PATH) as file:
+    template_config = yaml.safe_load(file)
+
 
 def main() -> None:
+    pdf = ResumePdf(template_config)
     for line in resume_lines:
         line = line.strip()
-        line = line.replace(" - ", " – ")
-        if line.startswith("# "):
-            print("h1:", line[2:])
+        # TODO: add with unicode font
+        # line = line.replace(" - ", " – ")
+        line = line.replace(" - ", " -- ")
+
+        is_comment = line.startswith("<!--") and line.endswith("-->")
+        if is_comment:
+            continue
+
+        if line == "":
+            pdf.line_break()
+        elif line.startswith("# "):
+            pdf.h1(line[2:])
         elif line.startswith("## "):
-            print("h2:", line[3:])
+            pdf.h2(line[3:])
         elif line.startswith("- "):
-            print("bull:", line[2:])
-        elif line == "":
-            print("(empty)")
-        elif line == "---":
-            print("----- line -----")
+            pdf.bullet(line[2:])
         else:
-            print("body: ", end="")
+            pdf.body(line)
 
-            if "**" in line:
-                new_line = ""
-                parts = line.split("**")
-                for idx, part in enumerate(parts):
-                    if idx % 2 == 1:
-                        new_line += f"<b>{part}</b>"
-                    else:
-                        new_line += part
-                line = new_line
-
-            if " >> " in line:
-                left, right = line.split(" >> ", 1)
-                line = f"<l>{left}</l><r>{right}</r>"
-
-            print(line)
+    pdf.output("resume.pdf")
 
 
 if __name__ == "__main__":
