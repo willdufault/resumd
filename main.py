0b33e1a075d317786b65547b04a059103dc48be9
wduf <wduf02@gmail.com>
2026-03-20 20:42:56 -0400

    refactor(main): read template name from sys argv

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+import sys
+
+from rich import print
+
 from models.config_parser import ConfigParser
 from models.font_fetcher import FontFetcher
 from models.layout_calculator import LayoutCalculator
-from models.resume_renderer import ResumeRenderer
 from models.resume_pdf import ResumePdf
 from models.resume_pdf_factory import ResumePdfFactory
+from models.resume_renderer import ResumeRenderer
 
-# TODO: read paths from sys.argv
-RESUME_PATH = "templates/default.md"
+TEMPLATE_NAME = sys.argv[1]
 
 
 def main() -> None:
+    print("Reading template...", flush=True, end="")
+    template_path = f"templates/{TEMPLATE_NAME}.md"
+    try:
+        with open(template_path) as file:
+            resume_lines = file.readlines()
+    except OSError:
+        print("❌")
+        print(f"[red]Error reading {template_path}[/red]")
+        sys.exit()
+
+    print("✅")
+
     print("Parsing config...", flush=True, end="")
-    with open(RESUME_PATH) as file:
-        resume_lines = file.readlines()
     config, closing_index = ConfigParser.parse(resume_lines)
     resume_data = resume_lines[closing_index + 1 :]
     print("✅")
